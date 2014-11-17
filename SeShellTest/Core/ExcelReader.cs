4dd8c5355650e56772d87d3b6f1f64c2cf827cc9
zaurik <a.ruzaik@gmail.com>
2014-11-17 16:41:03 +0530

    commented using statement for ClosedXML

--- SeShellTest/Core/ExcelReader.cs
+++ SeShellTest/Core/ExcelReader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ClosedXML.Excel;
+//using ClosedXML.Excel;
 
 namespace SeShell.Test.Core
 {
