3445dd1b232c462d7f1dc1f43a2f145620ef544f
zaurik <a.ruzaik@gmail.com>
2014-11-19 15:38:23 +0530

    commented lines from bat

--- DeploymentScripts/SeShellAutomationFramework - Copy.bat
+++ DeploymentScripts/SeShellAutomationFramework - Copy.bat
@@ -4,10 +4,10 @@ cd SeShellTest
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe SeShell.Test.csproj /p:configuration=debug
 cd bin\debug
 "C:\Users\ruzaika\Downloads\Libraries\NUnit-2.6.3\bin\nunit-console.exe" /fixture:SeShell.Test.TestCases.TestSuite SeShell.Test.dll
-cd ..
-cd ..
-cd ..
-cd SeShell.Test.XMLTestResult\bin\Debug\
-SeShell.Test.XMLTestResult.exe
-del /s C:\SeShellResult\*.xml 
-cd ..\..\..\DeploymentScripts
+:: cd ..
+:: cd ..
+:: cd ..
+:: cd SeShell.Test.XMLTestResult\bin\Debug\
+:: SeShell.Test.XMLTestResult.exe
+:: del /s C:\SeShellResult\*.xml 
+:: cd ..\..\..\DeploymentScripts
