dc6b4daa57ac4639d4a914c533602bb1656ded40
zaurik <a.ruzaik@gmail.com>
2015-05-18 12:15:59 +0530

    Updated DeploymentScripts/SeShellAutomationFramework.bat and changes browser to Chrome

--- DeploymentScripts/SeShellAutomationFramework.bat
+++ DeploymentScripts/SeShellAutomationFramework.bat
@@ -3,7 +3,7 @@ cd ..
 cd SeShellTest
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe SeShell.Test.csproj /p:configuration=debug
 cd bin\debug
-nunit-console /fixture:SeShell.Test.TestCases.TestSuite SeShell.Test.dll
+nunit-console-x86 /fixture:SeShell.Test.TestCases.TestSuite SeShell.Test.dll
 cd ..
 cd ..
 cd ..
